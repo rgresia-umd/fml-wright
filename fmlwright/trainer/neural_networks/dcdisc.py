36cb267868b5153e89bf3c06b851d127d31ffee2
rgresia <45833466+rgresia@users.noreply.github.com>
2021-05-08 10:15:28 -0400

    Update dcdisc.py

--- fmlwright/trainer/neural_networks/dcdisc.py
+++ fmlwright/trainer/neural_networks/dcdisc.py
@@ -1,15 +1,31 @@
-def make_discriminator_model():
-    model = tf.keras.Sequential()
-    model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same',
-                                     input_shape=[28, 28, 1]))
-    model.add(layers.LeakyReLU())
-    model.add(layers.Dropout(0.3))
+import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.keras.layers import (
+    Conv2D,
+    Dropout,
+    LeakyReLU,
+    BatchNormalization,
+    Sequential,
+    Dense,
+    Flatten,
+)
 
-    model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
-    model.add(layers.LeakyReLU())
-    model.add(layers.Dropout(0.3))
+import logging
 
-    model.add(layers.Flatten())
-    model.add(layers.Dense(1))
+log = logging.getLogger(__name__)
+
+def create_dcdisc(input_shape, dropout):
+    model = Sequential()
+    model.add(Conv2D(64, (5, 5), strides=(2, 2), padding='same',
+                                     input_shape=input_shape))
+    model.add(LeakyReLU())
+    model.add(Dropout(dropout))
+
+    model.add(Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
+    model.add(LeakyReLU())
+    model.add(Dropout(dropout))
+
+    model.add(Flatten())
+    model.add(Dense(1))
 
     return model
