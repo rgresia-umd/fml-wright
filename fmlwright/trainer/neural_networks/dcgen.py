8a83eaa2f30c1a343048338a8c70e505a147cd37
rgresia <45833466+rgresia@users.noreply.github.com>
2021-05-08 10:31:37 -0400

    Update dcgen.py

--- fmlwright/trainer/neural_networks/dcgen.py
+++ fmlwright/trainer/neural_networks/dcgen.py
@@ -1,25 +1,41 @@
 
+import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.keras.layers import (
+    Conv2DTranspose,
+    Dropout,
+    LeakyReLU,
+    BatchNormalization,
+    Sequential,
+    Dense,
+    Flatten,
+    Reshape,
+)
 
-def make_generator_model():
-    model = tf.keras.Sequential()
-    model.add(layers.Dense(7*7*256, use_bias=False, input_shape=(100,)))
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
+import logging
 
-    model.add(layers.Reshape((7, 7, 256)))
-    assert model.output_shape == (None, 7, 7, 256)  # Note: None is the batch size
+log = logging.getLogger(__name__)
 
-    model.add(layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
-    assert model.output_shape == (None, 7, 7, 128)
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
+def create_dcgen(input_shape, output_shape, dropout):
+    model = Sequential()
+    model.add(Dense((output_shape[0]/4)*(output_shape[1]/4)*(output_shape[2]*256), use_bias=False, input_shape=input_shape)
+    model.add(BatchNormalization())
+    model.add(LeakyReLU())
 
-    model.add(layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
-    assert model.output_shape == (None, 14, 14, 64)
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
+    model.add(Reshape(((output_shape[0]/4), (output_shape[1]/4), (output_shape[2]*256))))
+    assert model.output_shape == (None, (output_shape[0]/4), (output_shape[1]/4), (output_shape[2]*256))  # Note: None is the batch size
 
-    model.add(layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
-    assert model.output_shape == (None, 28, 28, 1)
+    model.add(Conv2DTranspose(output_shape[2]*128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
+    assert model.output_shape == (None, (output_shape[0]/4), (output_shape[1]/4), (output_shape[2]*128))
+    model.add(BatchNormalization())
+    model.add(LeakyReLU())
+
+    model.add(Conv2DTranspose(output_shape[2]*64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+    assert model.output_shape == (None, (output_shape[0]/2), (output_shape[1]/2), (output_shape[2]*64))
+    model.add(BatchNormalization())
+    model.add(LeakyReLU())
+
+    model.add(Conv2DTranspose(output_shape[2], (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
+    assert model.output_shape == (None, (output_shape[0]), (output_shape[1]), (output_shape[2]))
 
     return model
